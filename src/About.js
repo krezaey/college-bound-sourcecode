15f6be0824367f006ffb9104e59b65550ac86309
kckc1289 <keziah.camille8@ymail.com>
2020-12-05 18:23:39 -0800

    Prepared deployed site for user evaluation

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ function About() {
             <h1 style={{ marginTop: 50 }}>About</h1>
             <p>College Bound is a web application that aims to bridge the educational gap between first-generation college students and higher education. Despite recent improvements in accessibility in higher education, there is still more that needs to be done. This application provides plentiful information in order to make the college and financial aid application simpler. </p>
 
-            <p>This project was created by four members of LMU Computer Science class of 2022: Ameya Mellacheruvu, Keziah Rezaey, Elise Sawan, and Salem Tesfu. This group, known as PASTEL, is well aware of the educational inequalities that come with being first-generation students through first hand experience. They hope that their website can provide solace to prospective and current university students across the nation.</p>
+            {/* <p>This project was created by four members of LMU Computer Science class of 2022: Ameya Mellacheruvu, Keziah Rezaey, Elise Sawan, and Salem Tesfu. This group, known as PASTEL, is well aware of the educational inequalities that come with being first-generation students through first hand experience. They hope that their website can provide solace to prospective and current university students across the nation.</p> */}
         </div>
     );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -15,13 +15,13 @@ function Footer() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <Link to="/about">About</Link>
-                        <a href="https://github.com/krezaey/College_Bound/tree/master/sourcecode/college_bound">GitHub Repository</a>
+                        {/* <a href="https://github.com/krezaey/College_Bound/tree/master/sourcecode/college_bound">GitHub Repository</a> */}
                         <a href="https://www.shutterstock.com/g/GoodStudio">Art Credit</a>
                     </Nav>
                 </Navbar.Collapse>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
-                    <Link to="/applying_for_scholarships">
+                    <Link to="/applying_to_colleges">
                         <Button style={{ background: "#3B6280" }} type="submit">Submit</Button>
                     </Link>
                 </Form>
